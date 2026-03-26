11fe3147408b76df3489bb544c3d73b862c39348
Binuraj <73106149+binurajek@users.noreply.github.com>
2026-03-26 21:16:28 +0100

    swagger added

--- src/main/java/org/binz/microservicerefresher/controller/EmployeeController.java
+++ src/main/java/org/binz/microservicerefresher/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package org.binz.microservicerefresher.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.binz.microservicerefresher.model.Employee;
 import org.binz.microservicerefresher.service.EmployeeService;
@@ -12,30 +14,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
+@Tag(name = "Employee", description = "Employee management APIs")
 public class EmployeeController {
-    
+
     private final EmployeeService employeeService;
 
+    @Operation(summary = "Create a new employee")
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.createEmployee(employee), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get employee by ID")
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
         return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 
+    @Operation(summary = "Get all employees")
     @GetMapping
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
+    @Operation(summary = "Update an employee")
     @PutMapping("/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
         return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
     }
 
+    @Operation(summary = "Delete an employee")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
